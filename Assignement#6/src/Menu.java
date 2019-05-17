3e8ac98c331a8364aa491fb5e43a2261a397c597
mzuels <margaretzuelsdorf@gmail.com>
2019-05-17 12:33:59 -0400

    changed the menu class, made menuiterator.

--- Assignement#6/src/Menu.java
+++ Assignement#6/src/Menu.java
@@ -1,4 +1,10 @@
 
 public class Menu {
-
+	static final int MAX_ITEMS = 6;
+	int numberOfItems = 0;
+	MenuItem[] menuItems;
+	
+	public Iterator createIterator() {
+		return new MenuIterator(menuItems);
+	}
 }
